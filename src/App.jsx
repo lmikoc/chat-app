46d41e79b02927b78ca2c233ece56413e7609bbe
Luka Mikoc <luka18.mikoc@gmail.com>
2023-09-14 22:10:29 +0200

    Test displaying a message

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Messages from "./components/Messages";
+import { randomColor, randomName } from "./utils/generateRandomNameAndColor";
+import styles from "../src/styles/Home.module.css";
 
 const App = () => {
+    const [messages, setMessages] = useState([
+        {
+            id: "1",
+            data: "This is a test message!",
+            member: {
+                id: "1",
+                clientData: {
+                    color: "blue",
+                    username: "bluemoon"
+                }
+            }
+        }
+    ]);
+    const [me, useMe] = useState({
+        username: randomName(),
+        color: randomColor()
+    });
     return (
-        <div>
-            <Messages />
-        </div>
+        <main className={styles.app}>
+            <div className={styles.appContent}>
+                <Messages />
+            </div>
+        </main>
     );
 };
 
