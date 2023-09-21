4c4f9ef5eaa9e110c932a84dd797fe02e1e1cfc2
Luka Mikoc <luka18.mikoc@gmail.com>
2023-09-21 21:22:18 +0200

    Add ScaleDrone

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,65 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Messages from "./components/Messages";
 import { randomColor, randomName } from "./utils/generateRandomNameAndColor";
 import styles from "../src/styles/Home.module.css";
 import Input from "./Input";
 import Members from "./components/Members";
 
+let drone = null;
+let room = null;
+
 const App = () => {
-    const [messages, setMessages] = useState([
-        {
-            id: "1",
-            data: process.env.REACT_APP_CHANNELID,
-            member: {
-                id: "1",
-                clientData: {
-                    color: "blue",
-                    username: "bluemoon"
-                }
-            }
-        }
-    ]);
-    const [me, useMe] = useState({
+    const [messages, setMessages] = useState([]);
+    const [me, setMe] = useState({
         username: randomName(),
         color: randomColor()
     });
-    const [members, setMembers] = useState([
-        {
-            id: "1",
-            clientData: {
-                color: "blue",
-                username: "bluemoon"
+    const [members, setMembers] = useState([]);
+    const messagesRef = useRef();
+    messagesRef.current = messages;
+    const membersRef = useRef();
+    membersRef.current = members;
+    const meRef = useRef();
+    meRef.current = me;
+    const connectToScaleDrone = () => {
+        drone = new window.Scaledrone(process.REACT_APP_CHANNELID, {
+            data: meRef.current
+        });
+        drone.on("open", (error) => {
+            if (error) {
+                return console.error(error);
             }
+            meRef.current.id = drone.clientId;
+            setMe(meRef.current);
+        });
+    };
+    useEffect(() => {
+        if (drone === null) {
+            connectToScaleDrone();
+            room = drone.subscribe("observable-chatAppTest");
+            room.on("message", (message) => {
+                const { data, member } = message;
+                setMessages([...messagesRef.current, message]);
+            });
+            room.on("members", (members) => {
+                setMembers([members]);
+            });
+            room.on("member_join", (member) => {
+                setMembers([...membersRef.current, member]);
+            });
+            room.on("member_leave", ({ id }) => {
+                const index = membersRef.current.findIndex((m) => m.id === id);
+                const newMembers = [...membersRef.current];
+                newMembers.splice(index, 1);
+                setMembers(newMembers);
+            });
         }
-    ]);
+    }, []);
     const onSendMessage = (message) => {
-        const newMessage = {
-            data: message,
-            member: {
-                clientData: me
-            }
-        };
-        setMessages([...messages, newMessage]);
+        drone.publish({
+            room: "observable-room",
+            message
+        });
     };
     return (
         <main className={styles.app}>
